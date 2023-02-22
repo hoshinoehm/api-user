d5deec80312e81f8ea824654a6e13a0451cd023d
hoshinoehm <hoshinoehm@gmail.com>
2023-02-22 11:27:37 -0300

    iniciando o projeto

---
+++ src/main/java/br/com/ehm/apiuser/ApiUserApplication.java
@@ -0,0 +1,13 @@
+package br.com.ehm.apiuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUserApplication.class, args);
+	}
+
+}
