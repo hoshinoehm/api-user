ae80bfd019289a1a27eab7ffc9c2052f298a026f
hoshinoehm <hoshinoehm@gmail.com>
2023-02-23 20:58:13 -0300

    tratamento de excecao para objeto nao encontrado

--- src/main/java/br/com/ehm/apiuser/repositories/UserRepository.java
+++ src/main/java/br/com/ehm/apiuser/repositories/UserRepository.java
@@ -1,8 +1,8 @@
 package br.com.ehm.apiuser.repositories;
 
-import br.com.ehm.apiuser.domain.User;
+import br.com.ehm.apiuser.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<Users, Integer> {
 
 }
