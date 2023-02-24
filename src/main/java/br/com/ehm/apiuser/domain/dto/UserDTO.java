1f7bfa6f797031e15c65685ea0b77b4c936cb6d1
hoshinoehm <hoshinoehm@gmail.com>
2023-02-23 22:05:00 -0300

    retornando dto para resource

---
+++ src/main/java/br/com/ehm/apiuser/domain/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package br.com.ehm.apiuser.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private Integer id;
+    private String name;
+    private String email;
+    @JsonIgnore
+    private String password;
+
+}
